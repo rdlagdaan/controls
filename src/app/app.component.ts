883569783c0cc7980270277e960254a15383a3f1
Randy Lagdaan <rdlagdaan@tua.edu.ph>
2017-11-07 12:20:25 -0800

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
+import { EJComponents } from 'ej-angular2';
+//import {SelectModule} from 'ng2-select';
+//import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  resize: boolean;
+  btndisplay: boolean;
+  selectIndex: number;
+
+  
+  @ViewChild('dialog') dialog: EJComponents <any,any>;
+    constructor() {
+    this.resize = false;
+    this.btndisplay = false;
+  }
+  //Button click event handler to open the ejDialog
+  onClick(event) {
+   this.btndisplay = false;
+   this.dialog.widget.element.ejDialog('open');
+   this.selectIndex = 1;
+  }
+  //Dialog close event handler
+  onClose(event) {
+    this.btndisplay = true;
+  }
+
+
+
+  public items:Array<string> = ['Amsterdam', 'Antwerp', 'Athens', 'Barcelona',
+  'Berlin', 'Birmingham', 'Bradford', 'Bremen', 'Brussels', 'Bucharest',
+  'Budapest', 'Cologne', 'Copenhagen', 'Dortmund', 'Dresden', 'Dublin',
+  'Düsseldorf', 'Essen', 'Frankfurt', 'Genoa', 'Glasgow', 'Gothenburg',
+  'Hamburg', 'Hannover', 'Helsinki', 'Kraków', 'Leeds', 'Leipzig', 'Lisbon',
+  'London', 'Madrid', 'Manchester', 'Marseille', 'Milan', 'Munich', 'Málaga',
+  'Naples', 'Palermo', 'Paris', 'Poznań', 'Prague', 'Riga', 'Rome',
+  'Rotterdam', 'Seville', 'Sheffield', 'Sofia', 'Stockholm', 'Stuttgart',
+  'The Hague', 'Turin', 'Valencia', 'Vienna', 'Vilnius', 'Warsaw', 'Wrocław',
+  'Zagreb', 'Zaragoza', 'Łódź'];
+
+private value:any = {};
+private _disabledV:string = '0';
+private disabled:boolean = false;
+
+private get disabledV():string {
+  return this._disabledV;
+}
+
+private set disabledV(value:string) {
+  this._disabledV = value;
+  this.disabled = this._disabledV === '1';
+}
+
+public selected(value:any):void {
+  console.log('Selected value is: ', value);
+}
+
+public removed(value:any):void {
+  console.log('Removed value is: ', value);
+}
+
+public typed(value:any):void {
+  console.log('New search input: ', value);
+}
+
+public refreshValue(value:any):void {
+  this.value = value;
+}
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { EJAngular2Module } from 'ej-angular2';
 import { AppComponent } from './app.component';
+import {SelectModule} from 'ng2-select';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,EJAngular2Module.forRoot(), 
+    SelectModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
